94e2c58fa14e040ecee533763815546986b4e71b
SiTotesZ <116477929+SiTotesZ@users.noreply.github.com>
2024-09-01 19:09:14 -0700

    Delete src/routes/challenges/index.js

--- src/routes/challenges/index.js
+++
@@ -1,8 +0,0 @@
-const challengesRouter = require("express").Router();
-
-const getChallengeList = require("../../controllers/challenges/GETChallengeList");
-const addChallenge = require("../../controllers/challenges/POSTChallenge");
-
-challengesRouter.route("/challenges").get(getChallengeList).post(addChallenge);
-
-module.exports = challengesRouter;
