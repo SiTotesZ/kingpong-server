1a461481eba7131a09d9de4a6061b2fb21c49811
SiTotesZ <116477929+SiTotesZ@users.noreply.github.com>
2024-09-01 19:09:00 -0700

    Delete src/routes/tournaments/index.js

--- src/routes/tournaments/index.js
+++
@@ -1,7 +0,0 @@
-const tournamentsRouter = require("express").Router();
-
-const getTournaments = require("../../controllers/tournaments/GETTournaments");
-
-tournamentsRouter.route("/tournaments").get(getTournaments);
-
-module.exports = tournamentsRouter;
