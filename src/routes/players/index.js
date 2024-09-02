cca3511969de9aad9b79ec2aad0d4277783e3c4f
SiTotesZ <116477929+SiTotesZ@users.noreply.github.com>
2024-09-01 19:08:39 -0700

    Delete src/routes/players/index.js

--- src/routes/players/index.js
+++
@@ -1,8 +0,0 @@
-const playersRouter = require("express").Router();
-
-const getPlayerList = require("../../controllers/players/GETPlayerList");
-const addPlayer = require("../../controllers/players/POSTPlayer");
-
-playersRouter.route("/players").get(getPlayerList).post(addPlayer);
-
-module.exports = playersRouter;
