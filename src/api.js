b33f7ed0c9de46fa72ef117622994d4393b21870
SiTotesZ <116477929+SiTotesZ@users.noreply.github.com>
2024-09-01 19:07:14 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -5,9 +5,6 @@ const PORT = process.env.PORT || 8080;
 const cors = require("cors");
 require("dotenv").config();
 const gamesRouter = require("./routes/games/index");
-const playersRouter = require("./routes/players/index");
-const challengesRouter = require("./routes/challenges/index");
-const tournamentsRouter = require("./routes/tournaments/index");
 
 app.use(express.json());
 app.use(cors());
@@ -16,17 +13,12 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL);
 
-const Game = require("./models/game");
-
 app.get("/", async (request, response) => {
   console.log("hey");
   response.send({ hello: "world" });
 });
 
 app.use("/.netlify/functions/api", gamesRouter);
-app.use("/.netlify/functions/api", playersRouter);
-app.use("/.netlify/functions/api", challengesRouter);
-app.use("/.netlify/functions/api", tournamentsRouter);
 
 // module.exports.handler = serverless(app);
 
